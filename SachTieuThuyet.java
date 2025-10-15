ececb035aa6c2c7f122ebebc085704dd5fa4c984
Dương Phước Quang <duongphuocquang133@gmail.com>
2025-10-16 00:14:46 +0700

    Hoàn thành bài tập Tuần 6 - Quang

---
+++ SachTieuThuyet.java
@@ -0,0 +1,28 @@
+package THLTHDT;
+
+public class SachTieuThuyet extends Sach {
+    private String theLoai;     // Ví dụ: Lãng mạn, Khoa học viễn tưởng
+    private boolean laSachSeries;
+
+    public SachTieuThuyet(String maSach, String tieuDe, String tacGia,
+                          int namXuatBan, int soLuong,
+                          String theLoai, boolean laSachSeries) {
+        super(maSach, tieuDe, tacGia, namXuatBan, soLuong);
+        this.theLoai = theLoai;
+        this.laSachSeries = laSachSeries;
+    }
+
+    public String getTheLoai() { return theLoai; }
+    public void setTheLoai(String theLoai) { this.theLoai = theLoai; }
+
+    public boolean isLaSachSeries() { return laSachSeries; }
+    public void setLaSachSeries(boolean laSachSeries) { this.laSachSeries = laSachSeries; }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(
+            " | Thể loại: %s | Series: %s",
+            theLoai, laSachSeries ? "Có" : "Không"
+        );
+    }
+}
